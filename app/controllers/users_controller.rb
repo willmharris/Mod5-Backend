d83d24f5d760489ec6340fdb029d66c2a793e27a
willmharris <willmharris0@gmail.com>
2020-04-27 10:27:43 -0400

    managing routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+
+    def index 
+        users = User.all 
+        payload = []
+        if (params[:type] == "leads")
+            payload = users.select {|user| user.account_type == 1}
+        elsif (params[:type] == "clients")
+            payload = users.select {|user| user.account_type == 2}
+        end 
+        render json: payload
+    end 
+
 end
