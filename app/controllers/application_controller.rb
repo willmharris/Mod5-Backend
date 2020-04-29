134e2ce94ee6fb7e263774b57a589e25dddf2619
willmharris <willmharris0@gmail.com>
2020-04-29 17:47:45 -0400

    server fetch request refactored to a single request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::API
+
+    def info 
+        users = User.all
+        leads = []
+        clients = []
+        users.each { |user| 
+            if (user.account_type == 1)
+                leads.push(user) 
+            elsif (user.account_type == 2)
+                clients.push(user)
+            end 
+        }
+        cases = Case.all 
+        info = {leads: leads, clients: clients, cases: cases}  
+        render json: info 
+    end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :cases
   resources :user_cases
   resources :users
+  get '/info', to: 'application#info'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
