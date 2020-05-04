8eecb63caacdd6c397cde95e46738114fe4b24e0
willmharris <willmharris0@gmail.com>
2020-05-04 10:29:41 -0400

    test version 1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_24_063540) do
+ActiveRecord::Schema.define(version: 2020_05_04_142842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2020_04_24_063540) do
     t.integer "entry_point"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
