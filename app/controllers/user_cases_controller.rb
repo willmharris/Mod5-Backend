63cd8711610a218894fc8b13e8d72c8e585a4686
willmharris <willmharris0@gmail.com>
2020-05-05 15:05:03 -0400

    user case fully done

--- app/controllers/user_cases_controller.rb
+++ app/controllers/user_cases_controller.rb
@@ -1,2 +1,8 @@
 class UserCasesController < ApplicationController
+    def update 
+        user_case = UserCase.find(params[:id])
+        data = JSON.parse(request.body.string)
+        user_case.update(data)
+        render json: user_case
+    end 
 end
